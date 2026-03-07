5183b005c8bc6c217c04c739ff9a32db429171ca
Sadiq Malik <sadiqmalik86501@gmail.com>
2026-03-07 15:56:55 +0530

    Ai_Chat_Boat

---
+++ 19_Hand_on_python_project/Ai_Chat_Bot.py/main.py
@@ -0,0 +1,38 @@
+import requests
+
+messages=[]
+
+def Ai_Chat_Bot(message):
+    global messages
+
+    messages.append({
+        "role":"user",
+        "content":message
+    })
+
+    r=requests.post("http://localhost:11434/api/chat ",
+                    json={
+                        "model":"gemma3",
+                        "message":message,
+                        "stream":False
+                    })
+    response=r.json()["message"]["content"]
+
+    message={
+        "role":"assistant",
+        "content":response,
+        "images":"null"
+    }
+
+    messages.append(message)
+    print(f"Jarvis:{response}")
+
+if __name__=="__main__":
+    print("Hello I am Jarvis: Can I  Help With You:")
+
+    while True:
+        question_query=input()
+        if question_query=="":
+            continue
+        Ai_Chat_Bot(question_query)
+        
